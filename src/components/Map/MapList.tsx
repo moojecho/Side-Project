636688710cfb3564f7cdc70b2881db2995f64503
moojecho <wpfh188@naver.com>
2023-02-15 17:46:15 +0900

    test: mapList #2
    test: mapList #2

--- src/components/Map/MapList.tsx
+++ src/components/Map/MapList.tsx
@@ -20,32 +20,37 @@ const MapList = () => {
       mapNum: "map1",
       mapLocation1: 37.566826,
       mapLocation2: 126.9786567,
-      mapLocationName: "",
+      mapLocationName: "서울특별시 중구 세종대로 110 서울특별시청",
     },
     {
       mapNum: "map2",
-      mapLocation1: 37.566826,
-      mapLocation2: 126.9786567,
+      mapLocation1: 37.557,
+      mapLocation2: 126.8518,
+      mapLocationName: "서울 강서구 화곡로61길 10 대한장어빌딩",
     },
     {
       mapNum: "map3",
       mapLocation1: 37.566826,
       mapLocation2: 126.9786567,
+      mapLocationName: "서울특별시 중구 세종대로 110 서울특별시청",
     },
     {
       mapNum: "map4",
       mapLocation1: 37.566826,
       mapLocation2: 126.9786567,
+      mapLocationName: "서울특별시 중구 세종대로 110 서울특별시청",
     },
     {
       mapNum: "map5",
       mapLocation1: 37.566826,
       mapLocation2: 126.9786567,
+      mapLocationName: "서울특별시 중구 세종대로 110 서울특별시청",
     },
     {
       mapNum: "map6",
       mapLocation1: 37.566826,
       mapLocation2: 126.9786567,
+      mapLocationName: "서울특별시 중구 세종대로 110 서울특별시청",
     },
   ]);
 
@@ -60,35 +65,50 @@ const MapList = () => {
         level: 5,
       };
       const map = new window.kakao.maps.Map(container, option);
+
+
+      // 주소로 좌표 변환--------------------------------------------------------------------
+      
+      // const geocoder = new window.kakao.maps.services.Geocoder();
+      // // 주소로 좌표를 검색합니다..
+      // geocoder.addressSearch(`${list.mapLocationName}`, function (result:any, status:any) {
+  
+      //   // 정상적으로 검색이 완료됐으면 
+      //   if (status === window.kakao.maps.services.Status.OK) {
+    
+      //     var coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+    
+      //     // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+      //     map.setCenter(coords);
+      //   }
+      // })
+
+
+      // 마커-------------------------------------------------------------
+      
+      let markerPosition = new window.kakao.maps.LatLng(
+        list.mapLocation1,
+        list.mapLocation2
+      );
+
+      // 마커를 생성
+      let marker = new window.kakao.maps.Marker({
+        position: markerPosition,
+      });
+
+      // 마커를 지도 위에 표시
+      marker.setMap(map);
     });
   }, []);
 
   return (
     <MapListLayout>
-      <MapCard>
-        <CatPosition id="map1" />
-        <PositionInformation>{`서울특별시 중구 세종대로 110 서울특별시청`}</PositionInformation>
-      </MapCard>
-      <MapCard>
-        <CatPosition id="map2" />
-        <PositionInformation />
-      </MapCard>
-      <MapCard>
-        <CatPosition id="map3" />
-        <PositionInformation />
-      </MapCard>
-      <MapCard>
-        <CatPosition id="map4" />
-        <PositionInformation />
-      </MapCard>
-      <MapCard>
-        <CatPosition id="map5" />
-        <PositionInformation />
-      </MapCard>
-      <MapCard>
-        <CatPosition id="map6" />
-        <PositionInformation />
-      </MapCard>
+      {exampleList.map((list:map)=>
+        (<MapCard>
+        <CatPosition id={list.mapNum}/>
+        <PositionInformation>{list.mapLocationName}</PositionInformation>
+      </MapCard>)
+      )}
     </MapListLayout>
   );
 };
